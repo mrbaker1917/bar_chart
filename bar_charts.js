ef3a76885f4b23711089a163b491d1184b933e8c
Mark R Baker <mrbaker1917@gmail.com>
2020-08-01 20:50:36 -0700

    began to create bars with js

--- bar_charts.js
+++ bar_charts.js
@@ -1,17 +1,25 @@
-function drawBarChart(data, options, element) {
-    element = document.getElementsByClassName("container");
+function drawBarChart(data, options) {
+    // element = document.getElementsByClassName("container");
     let chartHeight = options.height;
     let chartWidth = options.width;
+    let arr = [];
     for (let i of data) {
-
+        let id = 'bar' + String(i);
+        let h = String(i*90) + "px";
+        arr.push(document.getElementById(id).style.height=h);
+        arr.push(document.getElementById(id).innerHTML=String(i));
     }
-
-
+    return arr;
 };
 
-function changeBarHeight() {
-    let heightRaise = document.getElementById('bar1').style.height='450px';
-    let colorChange = document.getElementById('bar1').style.backgroundColor='maroon';
-    let numChange = document.getElementById('bar1').innerHTML='Biggest!';
-    return heightRaise, colorChange, numChange;
-};
+// function changeBarHeight() {
+//     let h = '450px';
+//     let h2 = '350px';
+//     let heightRaise = document.getElementById('bar1').style.height=h;
+//     let colorChange = document.getElementById('bar2').style.height=h2;
+//     let numChange = document.getElementById('bar3').innerHTML='Biggest Bar Ever!';
+//     let array = [heightRaise, colorChange, numChange];
+//     return array;
+// };
+
+// console.log(drawBarChart([5,3,2,4,1], {height: 500, width: "100%"} ))
