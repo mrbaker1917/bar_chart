dc62495c6e21029c8a3d3e9760931ea8d7636a8d
Mark R Baker <mrbaker1917@gmail.com>
2020-08-02 19:42:00 -0700

    rounded nums for output

--- bar_charts.js
+++ bar_charts.js
@@ -11,7 +11,7 @@ function drawBarChart(data, options) {
         let bar = document.createElement('div');
         bar.style.height = h;
         bar.className = "bar";
-        bar.innerHTML = String((i*90).toFixed(2));
+        bar.innerHTML = String((i*90).toFixed(0));
         bar.style.backgroundColor = "#" + randColor;
         arr.push(document.getElementById('barchart').append(bar));
     }
