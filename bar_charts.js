277989604f2ccc472f3aa15d08fbba91c6924d7a
Mark R. Baker <mrbaker1917@gmail.com>
2020-08-08 18:08:43 -0700

    Got color option working!

--- bar_charts.js
+++ bar_charts.js
@@ -1,28 +1,34 @@
 
 function drawBarChart(data, options, element) {
   element = document.getElementById("barchart");
-  let chartHeight = options.height;
+  let chartHeight = parseInt(options.height);
   element.style.height = chartHeight;
   let barMargin = options.barMargin;
   element.innerHTML = "";
   let h1 = Math.max(...data) * 1.1;
+  let colors = options.colors;
+  console.log(colors)
   let top = document.getElementById("top");
   top.innerText = "MaxValue:" + String((h1 * 1.1).toFixed());
   let arr = [];
-  let randColor = "";
   for (let i of data) {
-    randColor = Math.floor(Math.random() * 16777215).toString(16);
     let h = String((i / h1 * chartHeight)) + "px";
     let bar = document.createElement('div');
     bar.style.height = h;
     bar.style.marginRight = barMargin;
     bar.className = "bar";
     bar.innerHTML = "<h6>" + i + "</h6>";
-    bar.style.backgroundColor = "#" + randColor;
+    if (colors[0] == "") {
+      bar.style.backgroundColor = randColor();
+    } else {
+      bar.style.backgroundColor = colors[Math.floor(Math.random()*colors.length)];
+    }
     arr.push(element.append(bar));
   }
   return arr;
 };
+
+const randColor = () => {return "#" + Math.floor(Math.random() * 16777215).toString(16)};
 
 function makeTicks(str) {
   let li = document.createElement('li');
@@ -46,7 +52,7 @@ $(function () {
   $("h1").fadeIn(3000);
   let data = makeRandArr(20);
   console.log(data)
-  let options = {height: '500', width: '100%', barMargin: '1px'};
+  let options = {height: '500', width: '100%', barMargin: '1px', colors: randColor()};
   drawBarChart(data, options);
   $("div.bar").slideDown(3000, function () {
     $(this).animate({ width: "100%" }, 6000);
@@ -55,7 +61,10 @@ $(function () {
     let data = $("input#array").val().split(",");
     let chartHeight = $("input#chartHeight").val();
     let barMargin = $("input#barMargin").val();
-    let options = {height: chartHeight, width: '100%', barMargin: barMargin};
+    let colors = $("input#colors").val().split(",");
+    colors = colors.map(i => i.trim());
+    console.log(colors[0] == "");
+    let options = {height: chartHeight, width: '100%', barMargin: barMargin, colors: colors};
     drawBarChart(data, options);
     $("div.bar").slideDown(3000, function () {
       $(this).animate({ width: "100%" }, 6000);
