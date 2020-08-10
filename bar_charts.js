843eeaca541fa82592e7275c6f05966da6f8a695
Mark R. Baker <mrbaker1917@gmail.com>
2020-08-10 12:33:43 -0700

    simplified barchart color change mouseenter

--- bar_charts.js
+++ bar_charts.js
@@ -109,9 +109,6 @@ $(function () {
 
 $(document).ready(function() {
   $("div.bar").on("mouseenter", function() {
-    $(this).css("background-color", "#aaa");
-  });
-  $("div.bar").on("mouseleave", function() {
     $(this).css("background-color", randColor());
   })
 })
